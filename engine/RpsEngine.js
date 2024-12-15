73e0be2a830255482e0b93a02c902a5d7c65040f
MrXgroot <161230118+MrXgroot@users.noreply.github.com>
2024-12-15 15:07:03 +0530

    server update

--- engine/RpsEngine.js
+++ engine/RpsEngine.js
@@ -1,10 +1,8 @@
 class RpsEngine {
   constructor() {
-    this.winner = "";
     this.maxScore = 3;
     this.player1Score = 3;
     this.player2Score = 3;
-    this.winner = "none";
     this.validChoices = ["rock", "paper", "scissor"];
   }
 
@@ -13,36 +11,54 @@ class RpsEngine {
       Math.floor(Math.random() * this.validChoices.length)
     ];
   }
-
+  resetGame() {
+    this.player1Score = 3;
+    this.player2Score = 3;
+  }
   validateRPS(player1Choice, player2Choice = null) {
     if (!player1Choice) return;
     player2Choice = player2Choice ? player2Choice : this.getRandomChoice();
     const result = {
-      player1: player1Choice,
-      player2: player2Choice,
-      playerScore: this.player1Score,
-      opponentScore: this.player2Score,
+      player: {
+        player1: player1Choice,
+        player2: player2Choice,
+        playerScore: this.player1Score,
+        opponentScore: this.player2Score,
+      },
+      opponent: {
+        player1: player2Choice,
+        player2: player1Choice,
+        playerScore: this.player2Score,
+        opponentScore: this.player1Score,
+      },
     };
 
     if (player1Choice === player2Choice) {
-      result.state = "tie";
+      result.player.state = "tie";
+      result.opponent.state = "tie";
     } else if (
       (player1Choice === "rock" && player2Choice === "scissor") ||
       (player1Choice === "paper" && player2Choice === "rock") ||
       (player1Choice === "scissor" && player2Choice === "paper")
     ) {
-      result.state = "player1";
+      result.player.state = "player1";
+      result.opponent.state = "player2";
       this.player2Score--;
-      result.opponentScore--;
+      result.player.opponentScore--;
+      result.opponent.playerScore--;
     } else {
-      result.state = "player2";
+      result.player.state = "player2";
+      result.opponent.state = "player1";
       this.player1Score--;
-      result.playerScore--;
+      result.player.playerScore--;
+      result.opponent.opponentScore--;
     }
-    if (result.playerScore == 0) {
-      result.winner = "opponent";
-    } else if (result.opponentScore == 0) {
-      result.winner = "player";
+    if (result.player.playerScore == 0) {
+      result.player.winner = "opponent";
+      result.opponent.winner = "player";
+    } else if (result.player.opponentScore == 0) {
+      result.player.winner = "player";
+      result.opponent.winner = "opponent";
     }
     return result;
   }
--- server.js
+++ server.js
@@ -5,8 +5,15 @@ const cors = require("cors");
 const rps = require("./engine/RpsEngine.js");
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
-const allowedOrigins = ["https://meow-match-mrxgroot.vercel.app"];
+
+const allowedOrigins = ["http://localhost:5173"];
+
+const io = new Server(server, {
+  cors: {
+    origin: allowedOrigins,
+    methods: ["GET", "POST"],
+  },
+});
 
 app.use(
   cors({
@@ -15,11 +22,11 @@ app.use(
     credentials: true,
   })
 );
-
 app.use(express.json());
 const games = {};
+const players = [];
 const getGameId = () => {
-  return Date.now();
+  return "" + Date.now();
 };
 
 io.on("connection", (socket) => {
@@ -27,32 +34,123 @@ io.on("connection", (socket) => {
 
   socket.on("createRoom", () => {
     const gameId = getGameId();
-    games[gameId] = { engine: new rps(), player1: socket.id, player2: null };
+    console.log(gameId);
+    if (socket.gameId) return;
+    games[gameId] = {
+      engine: new rps(),
+      player1: socket,
+      player2: null,
+      player1Choice: null,
+      player2Choice: null,
+    };
     socket.join(gameId);
+    socket.gameId = gameId;
+    socket.role = "player1";
     socket.emit("roomCreated", { gameId });
+    console.log("room created with id", gameId);
   });
 
   socket.on("joinRoom", (gameId) => {
-    console.log(gameId);
+    console.log("join request", gameId);
     const game = games[gameId];
     if (!game) {
       socket.emit("error", "no such rooms");
       return;
     }
+    if (game.player1 && game.player1.id === socket.id) return;
     if (game.player2 !== null) {
       socket.emit("error", "room full");
       return;
     }
-    game.player2 = socket.id;
+    game.player2 = socket;
+    socket.gameId = gameId;
+    socket.role = "player2";
     socket.join(gameId);
-    // console.log(game);
     if (game.player1 && game.player2) {
       io.to(gameId).emit("gameStarted", gameId);
     }
   });
 
+  socket.on("joinRandom", () => {
+    if (players.includes(socket)) return;
+    if (socket.gameId) socket.leave(socket.gameId);
+    console.log("join random");
+    if (players.length > 0) {
+      const player1 = players.shift();
+      if (!player1.connected) return;
+      const player2 = socket;
+      const gameId = getGameId();
+      games[gameId] = {
+        engine: new rps(),
+        player1,
+        player2,
+        player1Choice: null,
+        player2Choice: null,
+      };
+      player1.gameId = gameId;
+      player2.gameId = gameId;
+      player1.join(gameId);
+      player2.join(gameId);
+      io.to(gameId).emit("resetGame");
+      io.to(gameId).emit("gameStarted");
+    } else {
+      players.push(socket);
+    }
+  });
+
+  socket.on("playAgain", () => {
+    console.log(socket.id);
+    const game = games[socket.gameId];
+    if (!game) return;
+    socket.playAgain = true;
+    const { player1, player2 } = game;
+    if (player1.playAgain && player2.playAgain) {
+      game.engine.resetGame();
+      io.to(socket.gameId).emit("resetGame");
+      player1.playAgain = false;
+      player2.playAgain = false;
+    }
+  });
+
+  socket.on("validateRps", (playerChoice, callBack) => {
+    const gameId = socket.gameId;
+    const game = games[gameId];
+
+    if (!game) return;
+
+    if (socket === game.player1) {
+      game.player1Choice = playerChoice;
+    } else if (socket === game.player2) {
+      game.player2Choice = playerChoice;
+    } else {
+      return;
+    }
+
+    if (game.player1Choice != null && game.player2Choice != null) {
+      const result = game.engine.validateRPS(
+        game.player1Choice,
+        game.player2Choice
+      );
+
+      game.player1.emit("validateSuccessfull", result.player);
+      game.player2.emit("validateSuccessfull", result.opponent);
+
+      game.player1Choice = null;
+      game.player2Choice = null;
+    }
+  });
+
   socket.on("disconnect", () => {
     console.log(socket.id, "disconnected");
+    const gameId = socket.gameId;
+    //managing if in game leave
+    if (!gameId || !games[gameId]) return;
+    io.to(socket.gameId).emit("disconnected");
+    delete games[gameId];
+
+    //managing the players queue
+    const index = players.indexOf(socket);
+    if (index > -1) players.splice(index, 1);
   });
 });
 
